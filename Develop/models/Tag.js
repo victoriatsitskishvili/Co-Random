5675c34d3bcb6e85c9798f768d5d4ce063cb7094
victoriatsitskishvili <victoria.tsitskishvili@gmail.com>
2022-01-21 22:56:16 -0500

    tag model set up

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -6,7 +6,21 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id: {
+      //defining a new model using Sequelize.define//
+      type: DataTypes.INTEGER,
+      //Doesn't allow null values//
+      allowNull: false,
+      //Set as primary key//
+      primaryKey: true,
+      //Uses auto increment//
+      autoIncrement: true,
+    },
+    tag_name:
+    //String//
+     {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
