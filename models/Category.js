1934edb3c8fcdde10c52867903b62479661daec7
victoriatsitskishvili <victoria.tsitskishvili@gmail.com>
2022-01-22 11:20:38 -0500

    rearranging the folders and testing before submission

---
+++ models/Category.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class Category extends Model {}
+
+Category.init(
+  {
+    //to define the categoty model//
+    id: {
+      //defining a new model using Sequelize.define//
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
