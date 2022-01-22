7fe0e9a2ec792d79e3341ef544079398690031b5
victoriatsitskishvili <victoria.tsitskishvili@gmail.com>
2022-01-21 23:02:07 -0500

    ProductTag model set up

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -4,9 +4,40 @@ const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
+//// set up fields and rules for ProductTag model//
 ProductTag.init(
   {
-    // define columns
+    id: {
+      //defining a new model using Sequelize.define//
+      //Integer//
+      type: DataTypes.INTEGER,
+      //Doesn't allow null values//
+      allowNull: false,
+      //Set as primary key//
+      primaryKey: true,
+      //Uses auto increment//
+      autoIncrement: true,
+    },
+    product_id: {
+      //defining a new model using Sequelize.define//
+      //Integer//
+      type: DataTypes.INTEGER,
+      //References the product model's id//
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      //defining a new model using Sequelize.define//
+      //Integer//
+      type: DataTypes.INTEGER,
+      //References the tag model's id//
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
