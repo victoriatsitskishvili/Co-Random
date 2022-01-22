af6ffe2c94f85d0bdb689c90da21abe9d4bcf138
victoriatsitskishvili <victoria.tsitskishvili@gmail.com>
2022-01-21 23:09:00 -0500

    relationships between category, products and tags

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  //if a record in the parent table is deleted, then the corresponding records in the child table will automatically be deleted. This is called a cascade delete.//
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  //product_tags//
+  foreignKey: 'product_id'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  //product_tags//
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
